135f31f575ca587b38a82b17587ae117c64ac549
Maxim Maslin <zapzarap@yandex.ru>
2022-03-03 16:00:51 +0300

    wip

--- postgres_to_es/app/entrypoint.sh
+++ postgres_to_es/app/entrypoint.sh
@@ -14,6 +14,7 @@ fi
 python manage.py flush --no-input
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py create_movies
+#python manage.py create_movies
+python sqlite_to_posgres/load_data.py
 
 exec "$@"
