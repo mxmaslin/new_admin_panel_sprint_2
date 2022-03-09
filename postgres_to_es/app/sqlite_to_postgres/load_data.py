36442b924e12ba1c5055349f9092bd0f8b78ea85
Maxim Maslin <zapzarap@yandex.ru>
2022-03-10 00:48:46 +0300

    move creation of etl to sql

--- postgres_to_es/app/sqlite_to_postgres/load_data.py
+++ postgres_to_es/app/sqlite_to_postgres/load_data.py
@@ -25,14 +25,5 @@ if __name__ == '__main__':
     with sqlite3.connect(os.path.join(CURRENT_DIR, 'db.sqlite')) as sqlite_conn, psycopg2.connect(
         **dsl, cursor_factory=DictCursor
     ) as pg_conn:
-        query = [
-            "create role etl with password '123qwe'",
-            'alter role etl with login',
-            # 'grant all privileges on all tables in schema public to etl',
-            'grant all privileges on all tables in schema content to etl',
-            # 'alter role etl set search_path to content, public',
-        ]
-        query = ';'.join(query)
-        pg_conn.cursor().execute(query)
         load_from_sqlite(sqlite_conn, pg_conn)
     sqlite_conn.close()
